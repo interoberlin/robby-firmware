1dad57e997a9b3a18923a7cb363b1b378b631150
Matthias Bock <mail@matthiasbock.net>
2018-05-20 18:39:44 +0200

    Implemented Motor class

---
+++ motor.hpp
@@ -0,0 +1,45 @@
+
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
+
+#include <stdint.h>
+
+
+class Motor
+{
+private:
+    uint8_t pin1;
+    uint8_t pin2;
+
+public:
+    Motor(uint8_t pin1, uint8_t pin2)
+    {
+        // Remember pin numbers
+        this->pin1 = pin1;
+        this->pin2 = pin2;
+
+        // Configure GPIO as outputs
+        pinMode(pin1, OUTPUT);
+        pinMode(pin2, OUTPUT);
+    }
+    
+    void stop()
+    {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, LOW);
+    }
+
+    void forward()
+    {
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, LOW);
+    }
+
+    void reverse()
+    {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, HIGH);
+    }
+};
+
+#endif
--- robbie-firmware.ino
+++ robbie-firmware.ino
@@ -1,13 +1,37 @@
 
 
+/*
+ * Pinout for L298 motor driver
+ */
+#define PIN_L298_IN1  8
+#define PIN_L298_IN2  9
+#define PIN_L298_IN3 10
+#define PIN_L298_IN4 11
+
+
+#include "motor.hpp"
+
+Motor motorLeft(PIN_L298_IN2, PIN_L298_IN1);
+Motor motorRight(PIN_L298_IN3, PIN_L298_IN4);
+
+
 void setup()
 {
   Serial.begin(9600);
+  motorLeft.stop();
+  motorRight.stop();
 }
 
 void loop()
 {
   Serial.write("Hello world!\n");
-//  usleep(1000);
+  delay(1000);
+  motorLeft.forward();
+  delay(1000);
+  motorLeft.stop();
+  delay(1000);
+  motorRight.forward();
+  delay(1000);
+  motorRight.stop();
 }
 
